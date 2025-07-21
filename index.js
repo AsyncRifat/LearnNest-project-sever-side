7f7fd5d38c385ccf72ea7da1342c94f757d6c3e7
AsyncRifat <ibrahim3rifat@gmail.com>
2025-07-21 13:57:48 +0600

    get all class for teacher dashbord

--- index.js
+++ index.js
@@ -299,6 +299,35 @@ async function run() {
       }
     );
 
+    // get all class
+    app.get(
+      '/get-all-classes/:email',
+      verifyFirebaseToken,
+      async (req, res) => {
+        const email = req.params.email;
+
+        try {
+          if (!email) {
+            return res.status(400).send({ message: 'Email is required' });
+          }
+
+          const result = await classCollection.find({ email }).toArray();
+          if (result.length === 0) {
+            return res
+              .status(404)
+              .json({ message: 'No classes found for this email' });
+          }
+
+          res.status(200).json(result);
+        } catch (error) {
+          console.error('Error fetching classes:', error);
+          res
+            .status(500)
+            .json({ message: 'Internal Server Error', error: error.message });
+        }
+      }
+    );
+
     // add classes data save in database
     app.post(
       '/add-class',
